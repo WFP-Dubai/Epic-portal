b343decf0ec3a9301968f0e4a0656005511646c7
Tobias Carlander <tobias.carlander@wfp.org>
2012-11-05 12:32:31 +0400

    Fix Imports

--- plugins-sdk-6.1.0/hooks/userService-hook/docroot/WEB-INF/src/lu/globalepic/util/LiferayDBUtil.java
+++ plugins-sdk-6.1.0/hooks/userService-hook/docroot/WEB-INF/src/lu/globalepic/util/LiferayDBUtil.java
@@ -7,32 +7,6 @@ package lu.globalepic.util;
  * @author kaleem.mohammed
  *
  */
-import java.io.*;
-import java.net.URL;
-import java.util.Hashtable;
-
-import java.util.Enumeration;
-import java.util.List;
-
-import java.util.ArrayList;
-import javax.naming.*;
-import javax.naming.directory.*;
-
-import lu.globalepic.util.LDAPUserInfo;
-
-import com.liferay.portal.service.UserLocalServiceUtil;
-import com.liferay.portal.model.Contact;
-import com.liferay.portal.service.PhoneLocalServiceUtil;
-import com.liferay.portal.service.ListTypeServiceUtil;
-import com.liferay.portal.model.ListTypeConstants;
-import com.liferay.portal.model.ListType;
-import com.liferay.portal.model.User;
-import com.liferay.portal.model.Contact;
-import com.liferay.portal.model.Address;
-import com.liferay.portal.model.Phone;
-import com.liferay.portal.ModelListenerException;
-import com.liferay.portal.service.AddressLocalServiceUtil;
-import com.liferay.portal.util.PortalUtil;
 /*
 
  * Retrieve several attributes of a particular entry.
